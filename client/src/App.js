a4aab4cd80fbf34e0bbba874b065ce980f87bd2d
juandrouville <juandrouville>
2021-11-30 16:07:18 -0300

    renderizar navBar muestra

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,12 @@ import React from 'react';
 import { Route } from "react-router-dom";
 import Landing from "./Components/Landing/Landing";
 import Home from "./Components/Home/Home";
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+     <NavBar/>
      <Route exact path = "/" component={Landing}/>
      <Route path = "/home" component={Home}/>
     </div>
